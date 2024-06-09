089ae005f6f0837859d699987d413da3f38c5c98
sobirovmurodil <murodil@gmail.com>
2024-06-09 07:11:12 +0500

    add-todo-list-react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div className="max-container pt-10">
         <h1 className="text-white text-3xl text-center mb-3">Murodil's projects</h1>
         <p className="text-2xl text-white mb-8">My Works</p>  
-        <ul className="mt-10 bg-slate-700 p-5 h-full grid grid-cols-2 gap-5">
+        <ul className="mt-10 bg-slate-700 p-5 h-full grid grid-cols-2 gap-5 mb-12">
           <li>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
               <div className="card-body">
@@ -133,6 +133,25 @@ function App() {
                   </div>
                   <div>
                     <a href="https://dev-jobs-amber.vercel.app/">
+                      <button className="btn btn-secondary w-[110px]">Vercel</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title mb-5">8. TodoList in React Redux</h2>
+                <div className="card-actions flex items-center">
+                  <div className="mr-5">
+                    <a href="https://github.com/smurodil/todo-app-with-redux">
+                      <button className="btn btn-primary w-[110px]">Github</button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://todo-app-with-redux-tan.vercel.app/">
                       <button className="btn btn-secondary w-[110px]">Vercel</button>
                     </a>
                   </div>
